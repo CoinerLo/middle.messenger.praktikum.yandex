d7771da8546b40828a84f25e8f0f29e700d1676b
CoinerLo <coiner4677@gmail.com>
2022-09-09 20:08:06 +0300

    sprint_2 fix bug server.js

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const fallback = require('express-history-api-fallback');
-const path = require('path');
-const { fileURLToPath } = require('url');
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
